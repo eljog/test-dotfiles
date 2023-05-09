0c3d423740f1a1579b63aa2639039450863627d9
Eljo George <eljog@github.com>
2023-05-09 16:42:46 -0700

    Update install.sh to write envs

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 date >> ~/dotfiles_status
 echo "(dotfiles) starting ..." >> ~/dotfiles_status
 
-touch /tmp/.dotfilesMarker
+printenv > /tmp/.dotfilesMarker
 
 echo "(dotfiles) finished" >> ~/dotfiles_status
